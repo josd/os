d0dc88066febd83a644956863bd628bce7a976f9
josd <josderoo@gmail.com>
2025-03-05 00:13:22 +0100

    refreshing

--- etc/deontic-logic.pl
+++ etc/deontic-logic.pl
@@ -4,38 +4,38 @@
 :- op(1200, xfx, :+).
 
 :- discontiguous((:+)/2).
-:- discontiguous('urn:knows:not'/1).
-:- discontiguous('urn:knows:obligatory'/1).
-:- discontiguous('urn:knows:permitted'/1).
-:- discontiguous('urn:knows:forbidden'/1).
+:- discontiguous('urn:example:not'/1).
+:- discontiguous('urn:example:obligatory'/1).
+:- discontiguous('urn:example:permitted'/1).
+:- discontiguous('urn:example:forbidden'/1).
 
-:- dynamic('urn:knows:not'/1).
-:- dynamic('urn:knows:obligatory'/1).
-:- dynamic('urn:knows:permitted'/1).
-:- dynamic('urn:knows:forbidden'/1).
+:- dynamic('urn:example:not'/1).
+:- dynamic('urn:example:obligatory'/1).
+:- dynamic('urn:example:permitted'/1).
+:- dynamic('urn:example:forbidden'/1).
 
 % deontic logic rules
-'urn:knows:obligatory'(A) :+ 'urn:knows:not'('urn:knows:permitted'(B)), negation(B, A).
-'urn:knows:permitted'(A) :+ 'urn:knows:not'('urn:knows:obligatory'(B)), negation(B, A).
-'urn:knows:forbidden'(A) :+ 'urn:knows:obligatory'(B), negation(B, A).
+'urn:example:obligatory'(A) :+ 'urn:example:not'('urn:example:permitted'(B)), negation(B, A).
+'urn:example:permitted'(A) :+ 'urn:example:not'('urn:example:obligatory'(B)), negation(B, A).
+'urn:example:forbidden'(A) :+ 'urn:example:obligatory'(B), negation(B, A).
 
-'urn:knows:not'('urn:knows:obligatory'(A)) :+ 'urn:knows:obligatory'(B), negation(B, A).
-'urn:knows:not'('urn:knows:permitted'(A)) :+ 'urn:knows:forbidden'(A).
-'urn:knows:not'('urn:knows:forbidden'(A)) :+ 'urn:knows:permitted'(A).
+'urn:example:not'('urn:example:obligatory'(A)) :+ 'urn:example:obligatory'(B), negation(B, A).
+'urn:example:not'('urn:example:permitted'(A)) :+ 'urn:example:forbidden'(A).
+'urn:example:not'('urn:example:forbidden'(A)) :+ 'urn:example:permitted'(A).
 
-negation('urn:knows:not'(A), A) :- !.
-negation(A, 'urn:knows:not'(A)).
+negation('urn:example:not'(A), A) :- !.
+negation(A, 'urn:example:not'(A)).
 
 % examples
-'urn:knows:obligatory'(pay_taxes).
-'urn:knows:obligatory'(stop_at_red_light).
-'urn:knows:forbidden'(steal).
-'urn:knows:permitted'((drink, 'urn:knows:not'(drive))).
-'urn:knows:permitted'(('urn:knows:not'(drink), drive)).
-'urn:knows:not'('urn:knows:permitted'((drink, drive))).
+'urn:example:obligatory'(pay_taxes).
+'urn:example:obligatory'(stop_at_red_light).
+'urn:example:forbidden'(steal).
+'urn:example:permitted'((drink, 'urn:example:not'(drive))).
+'urn:example:permitted'(('urn:example:not'(drink), drive)).
+'urn:example:not'('urn:example:permitted'((drink, drive))).
 
 % query
-true :+ 'urn:knows:not'(_).
-true :+ 'urn:knows:obligatory'(_).
-true :+ 'urn:knows:permitted'(_).
-true :+ 'urn:knows:forbidden'(_).
+true :+ 'urn:example:not'(_).
+true :+ 'urn:example:obligatory'(_).
+true :+ 'urn:example:permitted'(_).
+true :+ 'urn:example:forbidden'(_).
