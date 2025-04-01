2b2802c3207160cb3ffc364f02122600ac0101de
josd <josderoo@gmail.com>
2025-04-01 22:45:43 +0200

    using < > quoted IRIs

--- input/socrates.pl
+++ input/socrates.pl
@@ -2,12 +2,12 @@
 
 :- op(1200, xfx, :+).
 
-:- dynamic('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'/2).
+:- dynamic('<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'/2).
 
-'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'('Socrates', '<urn:example:Man>').
+'<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('Socrates', '<urn:example:Man>').
 
-'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X, '<urn:example:Mortal>') :+
-    'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(X, '<urn:example:Man>').
+'<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'(X, '<urn:example:Mortal>') :+
+    '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'(X, '<urn:example:Man>').
 
 % query
-true :+ 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'(_, _).
+true :+ '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'(_, _).
